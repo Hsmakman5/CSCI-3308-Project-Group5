460067e421c4ae52ef0af1e55e5fc0308ff9e14b
Ethan Berk <Ethanwberk@gmail.com>
2024-04-04 14:00:29 -0600

    add login page test

--- ProjectSourceCode/test/server.spec.js
+++ ProjectSourceCode/test/server.spec.js
@@ -59,7 +59,19 @@ describe('Testing Add User API', () => {
       });
   });
 });
-
+describe('Testing Render', () => {
+  // Sample test case given to test /test endpoint.
+  it('test "/login" route should render with an html response', done => {
+    chai
+      .request(server)
+      .get('/login') // for reference, see lab 8's login route (/login) which renders home.hbs
+      .end((err, res) => {
+        res.should.have.status(200); // Expecting a success status code
+        res.should.be.html; // Expecting a HTML response
+        done();
+      });
+  });
+});
 
 
 // ********************************************************************************
