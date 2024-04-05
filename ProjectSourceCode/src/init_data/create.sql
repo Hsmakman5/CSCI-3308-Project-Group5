6166badf1bbb7efdb260368e2b94b37c6bfcfece
Ethan Berk <Ethanwberk@gmail.com>
2024-04-05 12:42:15 -0600

    add basic structure of database

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -1,7 +1,38 @@
 drop table if EXISTS users;
 CREATE TABLE users (
+        user_id INT PRIMARY KEY NOT NUll,
         username VARCHAR(50) PRIMARY KEY,
         password CHAR(60) NOT NULL
+);
+
+drop table if EXISTS movies;
+CREATE TABLE movies (
+        movie_id INT PRIMARY KEY NOT NUll,
+        movie_name = VARCHAR(100) NOT NUll,
+        duration SMALLINT NOT NUll,
+        language VARCHAR(30) NOT NUll,
+);
+
+
+drop table if EXISTS movie_recs;
+CREATE TABLE movie_recs (
+        movie_rec_id INT PRIMARY KEY NOT NUll,
+        weather VARCHAR(30) NOT NUll,
+        mood VARCHAR(30) NOT NUll,
+        time_of_day VARCHAR(30) NOT NUll,
+        likes SMALLINT NOT NULL
+);
+
+drop table if EXISTS movies_to_movie_recs;
+CREATE TABLE movies_to_movie_recs(
+        movie_id INT NOT NUll,
+        movie_rec_id INT NOT NUll
+);
+
+drop table if EXISTS users_to_movie_recs;
+CREATE TABLE movies_to_movie_recs(
+        user_id INT NOT NUll,
+        movie_rec_id INT NOT NUll
 );
 
 
