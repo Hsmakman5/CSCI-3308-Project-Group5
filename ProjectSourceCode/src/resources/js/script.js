1e9076aca11b2162ab5970b45b418de50c91b97a
Ethan Berk <Ethanwberk@gmail.com>
2024-04-15 15:23:54 -0600

    Finshed modal templete needs js

---
+++ ProjectSourceCode/src/resources/js/script.js
@@ -0,0 +1,21 @@
+const exampleModal = document.getElementById('exampleModal')
+
+console.log("test");
+if (exampleModal) {
+  exampleModal.addEventListener('show.bs.modal', event => {
+    // Button that triggered the modal
+    const button = event.relatedTarget
+    // Extract info from data-bs-* attributes
+    const movie = button.getAttribute('data-bs-whatever')
+    // If necessary, you could initiate an Ajax request here
+    // and then do the updating in a callback.
+
+    // Update the modal's content.
+    const modalTitle = exampleModal.querySelector('.modal-title')
+    const modalBodyInput = exampleModal.querySelector('.modal-body input')
+   
+
+    modalTitle.textContent = `${movie}`
+    modalBodyInput.value = recipient
+  })
+}
