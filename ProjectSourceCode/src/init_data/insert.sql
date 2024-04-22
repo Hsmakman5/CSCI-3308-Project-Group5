9f69806a6ecb6fe86d327c4b239b36c3e2e000b0
Henry Makman <Hema6223@colorado.edu>
2024-04-22 16:44:01 -0600

    notes

--- ProjectSourceCode/src/init_data/insert.sql
+++ ProjectSourceCode/src/init_data/insert.sql
@@ -9,6 +9,7 @@
 --     ('7', 'Extreme', 'Action');
 
 INSERT INTO movies (movie_id, movie_name, url)
+VALUES
     ('475557', 'Joker', '/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg'),
     ('244786', 'Whiplash', '/7fn624j5lj3xTme2SgiLCeuedmO.jpg'),
     ('335977', 'Indiana Jones and the Dial of Destiny', '/Af4bXE63pVsb2FtbW8uYIyPBadD.jpg'),
@@ -18,6 +19,7 @@ INSERT INTO movies (movie_id, movie_name, url)
     ('639933', 'The Northman', '/aSSJMnHknzKjlZ6zybwD7eyJ4Po.jpg');
 
 INSERT INTO movie_recs (movie_rec_id, weather, mood, watch_date)
+VALUES
     ('1', 'Rainy', 'Sad', ''),
     ('2', 'Cloudy', 'Neutral', ''),
     ('3', 'Sunny', 'Happy', ''),
@@ -27,6 +29,7 @@ INSERT INTO movie_recs (movie_rec_id, weather, mood, watch_date)
     ('7', 'Sunny', 'Happy', '');
 
 INSERT INTO movies_to_movie_recs (movie_id, movie_rec_id)
+VALUES
     ('475557','1'),
     ('244786', '2'),
     ('335977', '3'),
